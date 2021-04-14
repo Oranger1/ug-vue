4f23fcaad0b73e8ea95740fa52643d55871a7b85
Oranger7 <5394982+Oranger7@user.noreply.gitee.com>
2021-04-14 10:51:10 +0800

    地铁运营情况提交

---
+++ src/views/station/stationSitutation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+  
+</template>
+
+<script>
+import { pageTakeRecord,pageBill } from '../../services/billAndRecord/billAndRecordService';
+
+export default {
+  data () {
+    return {
+      
+    }
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
